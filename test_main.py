4114b618a0336453aa02b086d16f230df9743131
Javier Ibanez <JavierIbanez92@outlook.com>
2024-09-22 22:25:35 -0400

    Add greet_person function and tests

--- test_main.py
+++ test_main.py
@@ -1,9 +1,12 @@
 import unittest
-from main import hello_world
+from main import hello_world, greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hellooo!")
+        
+    def test_greet_person(self):
+        self.assertEqual(greet_person("Javier"), "Hello, Javier!")
 
 if __name__ == '__main__':
     unittest.main()
